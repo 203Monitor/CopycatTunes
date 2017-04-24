06dcf57700c7beb312bc6a857c35810f8900cf0e
203monitor <930502900@qq.com>
2017-04-24 18:36:25 +0800

    m 调整播放状态改变回调机制

--- CopycatTunes/YuriFramework/DouAudioStreamerUtil/DOUAudioUtil.h
+++ CopycatTunes/YuriFramework/DouAudioStreamerUtil/DOUAudioUtil.h
@@ -13,8 +13,12 @@ typedef void(^CallBack)(id obj);
 @interface DOUAudioUtil : NSObject
 
 @property (nonatomic, copy) void(^playingCallBack)(id obj);
-@property (nonatomic, copy) void(^startCallBack)(id obj);
+@property (nonatomic, copy) void(^pauseCallBack)(id obj);
 @property (nonatomic, copy) void(^finishCallBack)(id obj);
+
+- (void)setFinishCallBack:(void (^)(id obj))finishCallBack;
+- (void)setPlayingCallBack:(void (^)(id obj))playingCallBack;
+- (void)setPauseCallBack:(void (^)(id obj))pauseCallBack;
 
 - (void)playWithTrack:(id <DOUAudioFile>)track;
 - (void)play;
@@ -23,8 +27,11 @@ typedef void(^CallBack)(id obj);
 
 - (void)seek:(NSTimeInterval)timeInterval;
 
+- (BOOL)isPlaying;
+
 - (NSString *)duration;
 - (NSString *)currentTime;
+- (double)progress;
 - (NSString *)trackId;
 
 - (void)download;
--- CopycatTunes/YuriFramework/DouAudioStreamerUtil/DOUAudioUtil.m
+++ CopycatTunes/YuriFramework/DouAudioStreamerUtil/DOUAudioUtil.m
@@ -15,8 +15,6 @@
 @property (nonatomic, strong) DOUAudioStreamer *streamer;
 @property (nonatomic, strong) Track *track;
 
-@property (nonatomic, strong) NSTimer *timer;
-
 @end
 
 @implementation DOUAudioUtil
@@ -32,41 +30,64 @@
 
 - (instancetype)init {
     self = [super init];
+    
+    WS(weakSelf);
     if (self) {
         [[self rac_valuesAndChangesForKeyPath:@"streamer.status" options:NSKeyValueObservingOptionNew observer:nil] subscribeNext:^(id x) {
-            if ([[x first] integerValue] == DOUAudioStreamerFinished) {
-                [self setStreamer:nil];
-                if (self.finishCallBack) {
-                    dispatch_async(dispatch_get_main_queue(), ^{
-                        [self.timer invalidate];
-                        self.finishCallBack(nil);
-                    });
-                }
-            }
-            
-            if ([[x first] integerValue] == DOUAudioStreamerPaused) {
-                if (self.playingCallBack) {
-                    dispatch_async(dispatch_get_main_queue(), ^{
-                       [self.timer invalidate];
-                    });
-                }
-            }
-
-            if ([[x first] integerValue] == DOUAudioStreamerPlaying) {
-                if (self.playingCallBack) {
-                    dispatch_async(dispatch_get_main_queue(), ^{
-                        _timer = [NSTimer timerWithTimeInterval:1.0 target:self selector:@selector(timerFired) userInfo:nil repeats:YES];
-                        [[NSRunLoop currentRunLoop] addTimer:_timer forMode:NSDefaultRunLoopMode];
-                    });
-                }
+            NSInteger status = [[x first] integerValue];
+            switch (status) {
+                case DOUAudioStreamerPlaying:
+                    NSLog(@"player stated : playing");
+//                    [_buttonPlayPause setTitle:@"Pause" forState:UIControlStateNormal];
+                    if (weakSelf.playingCallBack) {
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            self.playingCallBack(self.streamer);
+                        });
+                    }
+                    
+                    break;
+                    
+                case DOUAudioStreamerPaused:
+                    NSLog(@"player stated : paused");
+//                    [_buttonPlayPause setTitle:@"Play" forState:UIControlStateNormal];
+                    if (self.pauseCallBack) {
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            self.pauseCallBack(nil);
+                        });
+                    }
+                    
+                    break;
+                    
+                case DOUAudioStreamerIdle:
+                    NSLog(@"player stated : idle");
+//                    [_buttonPlayPause setTitle:@"Play" forState:UIControlStateNormal];
+                    
+                    break;
+                    
+                case DOUAudioStreamerFinished:
+                    NSLog(@"player stated : finished");
+//                    [self _actionNext:nil];
+                    if (weakSelf.finishCallBack) {
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            self.finishCallBack(self.streamer);
+                        });
+                    }
+                    
+                    break;
+                    
+                case DOUAudioStreamerBuffering:
+                    NSLog(@"player stated : buffering");
+                    
+                    break;
+                    
+                case DOUAudioStreamerError:
+                    NSLog(@"player stated : error");
+                    
+                    break;
             }
         }];
     }
     return self;
-}
-
-- (void)timerFired {
-    self.playingCallBack(self.streamer);
 }
 
 - (DOUAudioStreamer *)streamer {
@@ -99,6 +120,10 @@
     [self.streamer setCurrentTime:(self.streamer.currentTime + timeInterval)];
 }
 
+- (BOOL)isPlaying {
+    return self.streamer.status == DOUAudioStreamerPlaying;
+}
+
 - (NSString *)duration {
     NSUInteger seconds = (NSUInteger)round(self.streamer.duration);
     NSString *string = [NSString stringWithFormat:@"%02lu:%02lu:%02lu",seconds / 3600, (seconds / 60) % 60, seconds % 60];
@@ -109,6 +134,14 @@
     NSUInteger seconds = (NSUInteger)round(self.streamer.currentTime);
     NSString *string = [NSString stringWithFormat:@"%02lu:%02lu:%02lu",seconds / 3600, (seconds / 60) % 60, seconds % 60];
     return string;
+}
+
+- (double)progress {
+    if (self.streamer.duration) {
+        return self.streamer.currentTime/self.streamer.duration;
+    }else {
+        return 0.0f;
+    }
 }
 
 - (NSString *)trackId {
